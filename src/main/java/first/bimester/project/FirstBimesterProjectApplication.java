c8cfebe9373466bc76aaab98a7fc3142c2ad9933
Michael Alexander <michael.lxndr@outlook.com>
2026-04-22 09:45:05 -0500

    Initial project setup: Add Spring Boot starter project with Gradle, wrapper scripts, ER diagram, and essential configuration files.

---
+++ src/main/java/first/bimester/project/FirstBimesterProjectApplication.java
@@ -0,0 +1,13 @@
+package first.bimester.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBimesterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBimesterProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/first/bimester/project/FirstBimesterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package first.bimester.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBimesterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
